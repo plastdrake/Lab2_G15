d1c2e4a1e11d94a1d5fa7d1a245b159b9f1b3373
Sebastian Svensson <sebastian@rosmic.se>
2025-01-06 15:37:41 +0100

    Fixing mainpage and adding viewmodel for mainpage

---
+++ ConcertBookingApp/ConcertBookingApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Input;
+using Microsoft.Maui.Controls;
+
+namespace ConcertBookingApp
+{
+    public class MainPageViewModel
+    {
+        public ICommand NavigateToConcertsCommand { get; }
+        public ICommand NavigateToBookingsCommand { get; }
+        public ICommand NavigateToAboutCommand { get; }
+
+        public MainPageViewModel()
+        {
+            NavigateToConcertsCommand = new Command(OnNavigateToConcerts);
+            NavigateToBookingsCommand = new Command(OnNavigateToBookings);
+            NavigateToAboutCommand = new Command(OnNavigateToAbout);
+        }
+
+        private async void OnNavigateToConcerts()
+        {
+            await Shell.Current.GoToAsync("//ConcertsPage");
+        }
+
+        private async void OnNavigateToBookings()
+        {
+            await Shell.Current.GoToAsync("//BookingsPage");
+        }
+
+        private async void OnNavigateToAbout()
+        {
+            await Shell.Current.GoToAsync("//AboutPage");
+        }
+    }
+}
