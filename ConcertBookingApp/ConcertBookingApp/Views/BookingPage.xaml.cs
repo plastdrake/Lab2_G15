d86b09c9373a5fc5ce038f9eb8a54f372028e557
Sebastian Svensson <sebastian@rosmic.se>
2025-01-07 10:42:04 +0100

    BookingPage WIP

--- ConcertBookingApp/ConcertBookingApp/Views/BookingPage.xaml.cs
+++ ConcertBookingApp/ConcertBookingApp/Views/BookingPage.xaml.cs
@@ -1,12 +1,29 @@
 using Microsoft.Maui.Controls;
+using ConcertBookingApp.Models;
+using ConcertBookingApp.ViewModels;
+using System.Linq;
 
 namespace ConcertBookingApp.Views
 {
+    [QueryProperty(nameof(ConcertId), "concertId")]
+    [QueryProperty(nameof(PerformanceId), "performanceId")]
     public partial class BookingPage : ContentPage
     {
+        public int ConcertId { get; set; }
+        public int PerformanceId { get; set; }
+
         public BookingPage()
         {
             InitializeComponent();
+            BindingContext = BookingViewModel.Instance;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Log to see the passed parameters
+            Console.WriteLine($"ConcertId: {ConcertId}, PerformanceId: {PerformanceId}");
         }
     }
 }
